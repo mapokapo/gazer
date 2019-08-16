7e1305009a9fd1a91ffb4ffa378d1c9f5e570f13
mapokapo <leopetrovic11@gmail.com>
2019-08-16 11:47:36 +0200

    update

--- functions/index.js
+++ functions/index.js
@@ -3,27 +3,43 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+exports.deleteUser = functions.https.onCall((data, context) => {
+  if (!context.auth) return {status: "error", code: 401, message: "Not signed in"}
+
+  return new Promise((resolve, reject) => {
+    admin.database().ref("users/").child(context.auth.uid).once("value", snapshot => {
+      if (snapshot.val() && snapshot.val().admin === 1) {
+        admin.auth().deleteUser(data.uid)
+          .then(() => {
+            resolve();
+          })
+          .catch(error => {
+            reject({status: "error", code: 500, error})
+          })
+      } else {
+        reject({status: "error", code: 403, message: "Forbidden"})
+      }
+    })
+  })
+});
+
 exports.getUser = functions.https.onCall((data, context) => {
   if (!context.auth) return {status: "error", code: 401, message: "Not signed in"}
 
   return new Promise((resolve, reject) => {
     // verify user"s rights
     admin.database().ref("users/").child(context.auth.uid).once("value", snapshot => {
-      if (snapshot.val().admin === 1) {
-        // query user data
-        admin.auth().getUser(data.uid)
-          .then(userRecord => {
-            let obj = userRecord;
-            delete obj.passwordHash;
-            resolve(obj.toJSON());
-          })
-          .catch(error => {
-            console.error("Error fetching user data:", error)
-            reject({status: "error", code: 500, error})
-          })
-      } else {
-        reject({status: "error", code: 403, message: "Forbidden"})
-      }
+      // query user data
+      admin.auth().getUser(data.uid)
+        .then(userRecord => {
+          let obj = userRecord;
+          delete obj.passwordHash;
+          resolve(obj.toJSON());
+        })
+        .catch(error => {
+          console.error("Error fetching user data:", error)
+          reject({status: "error", code: 500, error})
+        })
     })
   })
 })
